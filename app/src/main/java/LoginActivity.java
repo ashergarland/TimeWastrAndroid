3112e8ec99b963c1691221a7faf9325c773c1655
Zachary Wu <zawu@ucsd.edu>
2014-02-10 13:59:09 -0800

    uhhh

--- app/src/main/java/LoginActivity.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Zawu on 2/10/14.
- */
-public class LoginActivity {
-
-
-}
---
+++ app/src/main/java/timewastr/app/ArticleActivity.java
@@ -0,0 +1,7 @@
+package timewastr.app;
+
+/**
+ * Created by Zawu on 2/10/14.
+ */
+public class ArticleActivity {
+}
