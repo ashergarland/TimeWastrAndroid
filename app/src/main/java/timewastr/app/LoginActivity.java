f13bc9fb52f3d7063bcb4b2856dbdd93aa2e040e
Zachary Wu <zawu@ucsd.edu>
2014-02-25 13:10:49 -0800

    fixed compile errors, still need to do registration page, correct logic for connecting to the server for authetnication

--- app/src/main/java/timewastr/app/LoginActivity.java
+++ app/src/main/java/timewastr/app/LoginActivity.java
@@ -92,89 +92,91 @@ public class LoginActivity extends Activity {
         return true;
     }
 
-}
+    // file: LoginActivity.java
+    class LoginTask extends UrlJsonAsyncTask {
+        public LoginTask(Context context) {
+            super(context);
+        }
 
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(urls[0]);
+            JSONObject holder = new JSONObject();
+            JSONObject userObj = new JSONObject();
+            String response = null;
+            JSONObject json = new JSONObject();
 
-// file: LoginActivity.java
-class LoginTask extends UrlJsonAsyncTask {
-    public LoginTask(Context context) {
-        super(context);
-    }
-
-    @Override
-    protected JSONObject doInBackground(String... urls) {
-        DefaultHttpClient client = new DefaultHttpClient();
-        HttpPost post = new HttpPost(urls[0]);
-        JSONObject holder = new JSONObject();
-        JSONObject userObj = new JSONObject();
-        String response = null;
-        JSONObject json = new JSONObject();
-
-        try {
             try {
-                // setup the returned values in case
-                // something goes wrong
-                json.put("success", false);
-                json.put("info", "Something went wrong. Retry!");
-                // add the user email and password to
-                // the params
-                String mUserEmail = LoginActivity.mUserEmail;
-                String mUserPassword = LoginActivity.mUserPassword;
-                userObj.put("email", mUserEmail);
-                userObj.put("password", mUserPassword);
-                holder.put("user", userObj);
-                StringEntity se = new StringEntity(holder.toString());
-                post.setEntity(se);
+                try {
+                    // setup the returned values in case
+                    // something goes wrong
+                    json.put("success", false);
+                    json.put("info", "Something went wrong. Retry!");
+                    // add the user email and password to
+                    // the params
+//                    String mUserEmail = LoginActivity.mUserEmail;
+//                    String mUserPassword = LoginActivity.mUserPassword;
+                    userObj.put("email", mUserEmail);
+                    userObj.put("password", mUserPassword);
+                    holder.put("user", userObj);
+                    StringEntity se = new StringEntity(holder.toString());
+                    post.setEntity(se);
 
-                // setup the request headers
-                post.setHeader("Accept", "application/json");
-                post.setHeader("Content-Type", "application/json");
+                    // setup the request headers
+                    post.setHeader("Accept", "application/json");
+                    post.setHeader("Content-Type", "application/json");
 
-                ResponseHandler<String> responseHandler = new BasicResponseHandler();
-                response = client.execute(post, responseHandler);
-                json = new JSONObject(response);
+                    ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                    response = client.execute(post, responseHandler);
+                    json = new JSONObject(response);
 
-            } catch (HttpResponseException e) {
+                } catch (HttpResponseException e) {
+                    e.printStackTrace();
+                    Log.e("ClientProtocol", "" + e);
+                    json.put("info", "Email and/or password are invalid. Retry!");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("IO", "" + e);
+                }
+            } catch (JSONException e) {
                 e.printStackTrace();
-                Log.e("ClientProtocol", "" + e);
-                json.put("info", "Email and/or password are invalid. Retry!");
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.e("IO", "" + e);
+                Log.e("JSON", "" + e);
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
-            Log.e("JSON", "" + e);
+
+            return json;
         }
 
-        return json;
-    }
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                if (json.getBoolean("success")) {
+                    // everything is ok
+                    SharedPreferences mPreferences = LoginActivity.mPreferences;
+                    SharedPreferences.Editor editor = mPreferences.edit();
+                    // save the returned auth_token into
+                    // the SharedPreferences
+                    editor.putString("AuthToken", json.getJSONObject("data").getString("auth_token"));
+                    editor.commit();
 
-    @Override
-    protected void onPostExecute(JSONObject json) {
-        try {
-            if (json.getBoolean("success")) {
-                // everything is ok
-                SharedPreferences mPreferences = LoginActivity.mPreferences;
-                SharedPreferences.Editor editor = mPreferences.edit();
-                // save the returned auth_token into
-                // the SharedPreferences
-                editor.putString("AuthToken", json.getJSONObject("data").getString("auth_token"));
-                editor.commit();
-
-                // launch the HomeActivity and close this one
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
+                    // launch the HomeActivity and close this one
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                Toast.makeText(context, json.getString("info"), Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                // something went wrong: show a Toast
+                // with the exception message
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+            } finally {
+                super.onPostExecute(json);
             }
-            Toast.makeText(context, json.getString("info"), Toast.LENGTH_LONG).show();
-        } catch (Exception e) {
-            // something went wrong: show a Toast
-            // with the exception message
-            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
-        } finally {
-            super.onPostExecute(json);
         }
     }
+
+
 }
+
+
 
